ff7e0cb0c8887ba4c763d38cc750180673c272e9
Vefridda <82829175+Vefridda@users.noreply.github.com>
2021-07-24 01:27:16 +0700

    Initial commit

---
+++ settings.py
@@ -0,0 +1,3 @@
+
+valid_email = "dafna0504@yandex.ru"
+valid_password = "veretra7"
---
+++ tests/test_pet_friends.py
@@ -0,0 +1,36 @@
+from api import PetFriends
+from settings import valid_email, valid_password
+import os
+
+pf = PetFriends()
+
+
+def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
+    status, result = pf.get_api_key(email, password)
+    assert status == 200
+    assert 'key' in result
+
+def test_get_all_pets_with_valid_key(filter=''):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.get_list_of_pets(auth_key, filter)
+    assert status == 200
+    assert len(result['pets']) > 0
+
+def test_add_new_pet_info(name='Конфуций', animal_type='кот', age='7', pet_photo='images\cc56432da7e.jpg'):
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.add_new_pet_info(auth_key, name, animal_type, age, pet_photo)
+    assert status == 200
+    assert result['name'] == Конфуций
+
+def test_successful_delete_self_pet():
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    if len(my_pets['pets']) == 0:
+        pf.add_new_pet(auth_key, "Суперкот", "кот", "3", "images/cat1.jpg")
+        _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    pet_id = my_pets['pets'][0]['id']
+    status, _ = pf.delete_pet(auth_key, pet_id)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    assert status == 200
+    assert pet_id not in my_pets.values()
